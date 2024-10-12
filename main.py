c7140797e95606e8d156f1fac58c90e094a964f6
Mathew Miller <mathewpqmiller@gmail.com>
2024-10-11 17:52:17 -0700

    Finished setting up paddle parameters

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PONG")
+screen.tracer(0)
 
 paddle = Turtle()
 paddle.shape("square")
@@ -13,5 +14,25 @@ paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
 
+def go_up():
+    new_ycor = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_ycor)
+
+def go_down():
+    new_ycor = paddle.ycor() -20
+    paddle.goto(paddle.xcor(), new_ycor)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
+
 
 screen.exitonclick()
+
