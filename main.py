765e8f2791c87ca5459828af635aecabbd6ca697
Mathew Miller <mathewpqmiller@gmail.com>
2024-10-11 17:20:44 -0700

    Setting up Pong game screen parameters

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from turtle import Turtle
+
+
+turtle = Turtle()
+turtle.color("white")
+turtle.penup()
+screen.screensize(width=800, height=600)
+screen.bgcolor("black")
+
+
+
+
+
+
+exit.onscreenclick()
